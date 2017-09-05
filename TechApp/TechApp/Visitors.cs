f9a1fd106c15bbed21ba60c8c722f8ed004db05b
Oscar <oscar.smith.mps@gmail.com>
2017-09-05 16:04:54 +1200

    sync for sam

--- TechApp/TechApp/Visitors.cs
+++ TechApp/TechApp/Visitors.cs
@@ -38,9 +38,28 @@ namespace TechApp
 
                 try
                 {
+                    string query =
+                        "insert into tangible.tbltechVisitors(firstName,lastName,phoneNumber,emailAddress, appointmentTime, teacherID) values('" + this.firstname + "','" + this.lastname + "','" + this.phonenumber + "','" + this.emailaddress + "','" + this.appointmenttime+ "','" + this.teacherid + "';";
+                    MySqlCommand MyCommand2 = new MySqlCommand(query, cnn);
+                    MySqlDataReader queryReader;
+                    cnn.Open();
+                    queryReader = MyCommand2.ExecuteReader();
+                    MessageBox.Show("Save Data");
+                    while (queryReader.Read())
+                    {
 
+                    }
+
+                    cnn.Close();
+                }
+                finally
+                {
+                    cnn.Close();
                 }
 
+            }finally
+            {
+                cnn.Close();
             }
 
 
