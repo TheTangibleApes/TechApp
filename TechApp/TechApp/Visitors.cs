fcd2633701e348379a63a73b1f18e5e3cd4f764c
Oscar <oscar.smith.mps@gmail.com>
2017-09-05 15:46:57 +1200

    Adding the visitor class

---
+++ TechApp/TechApp/Visitors.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechApp
+{
+    class Visitors
+    {
+        string firstname;
+        string lastname;
+        string phonenumber;
+        string emailaddress;
+        DateTime appointmenttime;
+        int teacherid;
+
+        public void SetInformation(string FirstName, string LastName, string PhoneNumber, string EmailAddress, DateTime AppointmentTime, int TeacherID)
+        {
+            firstname = FirstName;
+            lastname = LastName;
+            phonenumber = PhoneNumber;
+            emailaddress = EmailAddress;
+            appointmenttime = AppointmentTime;
+            teacherid = TeacherID;
+        }
+
+        public void SubmitToDatabase()
+        {
+
+        }
+
+    }
+
+
+
+    
+}
