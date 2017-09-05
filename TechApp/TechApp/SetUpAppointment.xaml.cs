761bdc7ce46e0530009bb78d36121b406990c2b9
Caebyn <Caebyn@PCENG5084>
2017-09-05 14:26:10 +1200

    tht

---
+++ TechApp/TechApp/SetUpAppointment.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TechApp
+{
+    /// <summary>
+    /// Interaction logic for SetUpAppointment.xaml
+    /// </summary>
+    public partial class SetUpAppointment : Window
+    {
+        public SetUpAppointment()
+        {
+            InitializeComponent();
+        }
+    }
+}
