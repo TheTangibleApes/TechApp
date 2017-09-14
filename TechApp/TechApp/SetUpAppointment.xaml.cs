84307f2219056a896d96cae595fe1a254d43af0b
Caebyn <Caebyn@PCENG5084>
2017-09-14 14:02:57 +1200

    999

--- TechApp/TechApp/SetUpAppointment.xaml.cs
+++ TechApp/TechApp/SetUpAppointment.xaml.cs
@@ -74,5 +74,14 @@ namespace TechApp
             // Hide the MainWindow until later
             this.Close();
         }
+
+        private void prev_page_2(object sender, MouseButtonEventArgs e)
+        {
+            SignUpPage Apt1 = new SignUpPage();
+            Apt1.Show();
+
+            // Hide the MainWindow until later
+            this.Close();
+        }
     }
 }
